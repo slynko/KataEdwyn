0bb96ec4bc2f5bfbf6f1b1867a5b24404847ff40
slynko <oleksandr.o.slynko@gmail.com>
2022-02-15 16:21:49 +0100

    add first test and update readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 As a user I want to be able to select a coffee type and make it.
 
-Coffee types and ingredients:
+Coffee machine could be configured to accept different coffee recipes:
 - Espresso = 100% Espresso
 - Latte = 30% Espresso, 70% Milk
 - Americano = 30% Espresso, 70% Water
@@ -11,6 +11,7 @@ Coffee types and ingredients:
 
 - Feature #1: 
   - Coffee machine should be able to make an espresso
+    - Subtask #1: 
 
 - Feature #2: 
   - Coffee machine should be able to switch between coffee recipes (Espresso, Latte, Americano)
---
+++ src/test/java/tech/edwyn/CoffeeMachineTest.java
@@ -0,0 +1,24 @@
+package tech.edwyn;
+
+import org.junit.Test;
+import tech.edwyn.CoffeeMachine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CoffeeMachineTest {
+
+    @Test
+    public void shouldMakeCoffeeWithEspressoIngredient() {
+//        //given
+//        var coffeeMachine = new CoffeeMachine();
+//        var espressoRecipe = new EspressoRecipe();
+//        coffeeMachine.setRecipe(espressoRecipe);
+//
+//        //when
+//        var actualCoffee = coffeeMachine.makeCoffee();
+//
+//        //then
+//        assertThat(actualCoffee.getIngredients()).contains(ESPRESSO);
+    }
+}
